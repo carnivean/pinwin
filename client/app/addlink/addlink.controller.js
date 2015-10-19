880dac8f51eacbc350aba533a473e6741c488054
Erik Kynast <erikkynast@gmail.com>
2015-10-19 10:19:41 +0200

    MyPins working, but UI is kinda ugly right now...

--- client/app/addlink/addlink.controller.js
+++ client/app/addlink/addlink.controller.js
@@ -13,6 +13,30 @@ angular.module('pinApp')
     };
 
     $scope.inputChanged = function() {
+      if(checkURL($scope.input)) {
+        $scope.url = $scope.input;
+      }
+    };
+
+    $scope.submitLink = function() {
+        if ($scope.url != '') {
+          $scope.errorMessage = '';
+          var newEntry = {
+            username: [Auth.getCurrentUser().name],
+            url: $scope.url,
+            likes: [Auth.getCurrentUser().name]
+          };
+
+          $http.post('/api/links', newEntry)
+            .success(function(data){
+                $scope.message = ' Your link was successfully added. :-)';
+            })
+            .error(function(data) {
+                $scope.errorMessage = 'Something went wrong during the upload of your link, sorry :-(';
+            });
+        } else {
+          $scope.errorMessage = ' Please enter a link before submitting. Thanks!';
+        }
 
     };
   });
